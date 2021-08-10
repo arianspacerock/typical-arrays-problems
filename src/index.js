c0597514d3c941204ee586bc4a27bfafbbc1fc50
arianspacerock <siropemptyspace@gmail.com>
2021-08-10 20:09:49 +0300

    complete 3/5 task

--- src/index.js
+++ src/index.js
@@ -10,3 +10,20 @@ exports.max = function max (array) {
 exports.avg = function avg (array) {
   return 0;
 }
+
+function maxMinAvg(arr) {
+    var max = arr[0];
+    var min = arr[0];
+    var sum = arr[0];
+    for (var i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        sum = sum + arr[i];
+    }
+    var avg = sum / arr.length;
+    return [max, min, avg];
+}
